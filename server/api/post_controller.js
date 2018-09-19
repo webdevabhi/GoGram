3adaf0a8f039ee88c20672cda28a2e10094cbadd
webdevabhi <abhishek.patel185@gmail.com>
2018-09-19 22:26:39 +0530

    Infinite scrolling post

--- server/api/post_controller.js
+++ server/api/post_controller.js
@@ -1,26 +1,38 @@
 "use strict";
 
-const utilityFunc = requireInternal("utility");
-const post = requireInternal("models.post_model");
+var utilityFunc = requireInternal("utility");
+var post = requireInternal("models.post_model");
+var publicIp = require('public-ip');
 
 exports.index = index;
 exports.create = create;
 exports.deletePost = deletePost;
 
-// Gets a list of Posts
+/* 
+ * Gets a list of Posts 
+ * Inital get 25 data.
+ * get limit in query after scrolling client side page.
+ */
 function index(req, res) {
-  return post
-    .find({}).sort({ _id: -1 })
+  var limit = 25;
+  if(req.query.limit) limit = parseInt(req.query.limit);
+  publicIp.v4().then(ip => {
+    return post
+    .find({post_network_ip: ip}, { post_desc: 1, user_id: 1, post_image: 1 }).sort({ _id: -1 }).limit(limit)
     .exec().then(utilityFunc.respondWithResult(res)).catch(utilityFunc.handleError(res));
+  });
 }
 
 // Create a new post.
 function create(req, res) {
-  var reqData = req.body;
-  reqData.user_id = req.userId;
-  reqData.post_image = 'http://' + req.headers.host + '/uploads/images/' + req.file.filename;
+  publicIp.v4().then(ip => {
+    var reqData = req.body;
+    reqData.user_id = req.userId;
+    reqData.post_image = 'http://' + req.headers.host + '/uploads/images/' + req.file.filename;
+    reqData.post_network_ip = ip;
 
-  return post.create(reqData).then(utilityFunc.respondWithResult(res, 201)).catch(utilityFunc.handleError(res));
+    return post.create(reqData).then(utilityFunc.respondWithResult(res, 201)).catch(utilityFunc.handleError(res));
+  });
 }
 
 function deletePost(req, res){
--- server/models/post_model.js
+++ server/models/post_model.js
@@ -6,6 +6,7 @@ var PostSchema = new mongoose.Schema({
   post_image: {type: String, required: true},
   post_desc:  String,
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  post_network_ip: { type: String, required: true },
   created_at: { type: Date, default: Date.now },
   update_at: { type: Date, default: Date.now }
 })
